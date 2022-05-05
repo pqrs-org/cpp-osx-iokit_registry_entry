7cd26f6956dcc6e918daf7440dfb532d2d965a61
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-05 23:34:49 +0900

    Change get_root_entry argument type to iokit_mach_port::value_t from mach_port_t

--- include/pqrs/osx/iokit_registry_entry.hpp
+++ include/pqrs/osx/iokit_registry_entry.hpp
@@ -29,8 +29,8 @@ public:
   explicit iokit_registry_entry(const iokit_object_ptr& registry_entry) : registry_entry_(registry_entry) {
   }
 
-  static iokit_registry_entry get_root_entry(mach_port_t main_port = kIOMainPortDefault) {
-    return iokit_registry_entry(IORegistryGetRootEntry(main_port));
+  static iokit_registry_entry get_root_entry(iokit_mach_port::value_t port = iokit_mach_port::null) {
+    return iokit_registry_entry(IORegistryGetRootEntry(type_safe::get(port)));
   }
 
   //
